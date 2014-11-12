624aaad89b21128edf3d79399bc9b7ddd29223d9
isabol.foobar@gmail.com <isabgol.foobar@gmail.com>
2014-11-12 00:31:30 -0800

    Add build plan visitors that bring back features

---
+++ BuildPlanVisitors/TransientLifetimeRemovalBuildPlanVisitor.cs
@@ -0,0 +1,92 @@
+﻿namespace QuickInject.BuildPlanVisitors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using Microsoft.Practices.Unity;
+
+    public sealed class TransientLifetimeRemovalBuildPlanVisitor : IBuildPlanVisitor
+    {
+        public Expression Visitor(Expression expression, Type type, bool slowPath)
+        {
+            var deduper = new TransientLifetimeRemovalExpressionVisitor();
+            var emptyExpressionVisitor = new EmptyExpressionVisitor();
+            var modifiedExpression = emptyExpressionVisitor.Visit(deduper.Visit(expression));
+            return modifiedExpression;
+        }
+
+        private sealed class EmptyExpressionVisitor : ExpressionVisitor
+        {
+            protected override Expression VisitBlock(BlockExpression node)
+            {
+                var modifiedExpressionList = new List<Expression>();
+                foreach (var expression in node.Expressions)
+                {
+                    if (expression.NodeType == ExpressionType.Default && expression.Type == typeof(void))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        modifiedExpressionList.Add(expression);
+                    }
+                }
+
+                return base.VisitBlock(Expression.Block(node.Variables, modifiedExpressionList));
+            }
+        }
+
+        private sealed class TransientLifetimeRemovalExpressionVisitor : ExpressionVisitor
+        {
+            private static Type TransientLifetimeManagerType = typeof(TransientLifetimeManager);
+
+            private static MethodInfo SetValueMethodInfo = TransientLifetimeManagerType.GetRuntimeMethod("SetValue", new[] { typeof(object) });
+
+            protected override Expression VisitBlock(BlockExpression node)
+            {
+                var modifiedExpressionList = new List<Expression>();
+                foreach (var expression in node.Expressions)
+                {
+                    if (expression.NodeType == ExpressionType.Call)
+                    {
+                        var methodCall = (MethodCallExpression)expression;
+                        if (methodCall != null && methodCall.Object != null && methodCall.Object.Type == TransientLifetimeManagerType && methodCall.Method == SetValueMethodInfo)
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        modifiedExpressionList.Add(expression);
+                    }
+                }
+
+                return base.VisitBlock(Expression.Block(node.Variables, modifiedExpressionList));
+            }
+
+            protected override Expression VisitConditional(ConditionalExpression node)
+            {
+                var equalsOperation = node.Test as BinaryExpression;
+                if (equalsOperation != null)
+                {
+                    var assign = equalsOperation.Left as BinaryExpression;
+                    if (assign != null)
+                    {
+                        var typeAs = assign.Right as UnaryExpression;
+                        if (typeAs != null)
+                        {
+                            var methodCall = typeAs.Operand as MethodCallExpression;
+                            if (methodCall != null && methodCall.Object != null && methodCall.Object.Type == TransientLifetimeManagerType)
+                            {
+                                return base.Visit(node.IfTrue);
+                            }
+                        }
+                    }
+                }
+
+                return base.VisitConditional(node);
+            }
+        }
+    }
+}
---
+++ BuildPlanVisitors/TypeDeduplicationBuildPlanVisitor.cs
@@ -0,0 +1,97 @@
+﻿namespace QuickInject.BuildPlanVisitors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    public sealed class TypeDeduplicationBuildPlanVisitor : IBuildPlanVisitor
+    {
+        public Expression Visitor(Expression expression, Type type, bool slowPath)
+        {
+            var deduper = new TypeDeduplicationExpressionVisitor();
+            var emptyExpressionVisitor = new EmptyExpressionVisitor();
+            var modifiedExpression = emptyExpressionVisitor.Visit(deduper.Visit(expression));
+            return modifiedExpression;
+        }
+
+        private sealed class EmptyExpressionVisitor : ExpressionVisitor
+        {
+            protected override Expression VisitBlock(BlockExpression node)
+            {
+                var modifiedExpressionList = new List<Expression>();
+
+                foreach (var expression in node.Expressions)
+                {
+                    if (expression.NodeType == ExpressionType.Default && expression.Type == typeof(void))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        modifiedExpressionList.Add(expression);
+                    }
+                }
+
+                return base.VisitBlock(Expression.Block(node.Variables, modifiedExpressionList));
+            }
+        }
+
+        private sealed class TypeDeduplicationExpressionVisitor : ExpressionVisitor
+        {
+            private Dictionary<Type, ParameterExpression> typeToParameterExpressionTable = new Dictionary<Type, ParameterExpression>();
+
+            private HashSet<ParameterExpression> parameters = new HashSet<ParameterExpression>();
+
+            private bool topMostBlockExpressionVisited;
+
+            protected override Expression VisitBlock(BlockExpression node)
+            {
+                if (!this.topMostBlockExpressionVisited)
+                {
+                    foreach (var variable in node.Variables)
+                    {
+                        if (!this.typeToParameterExpressionTable.ContainsKey(variable.Type))
+                        {
+                            this.typeToParameterExpressionTable.Add(variable.Type, variable);
+                            this.parameters.Add(variable);
+                        }
+                    }
+
+                    this.topMostBlockExpressionVisited = true;
+                    return base.VisitBlock(Expression.Block(this.typeToParameterExpressionTable.Values, node.Expressions));
+                }
+
+                return base.VisitBlock(node);
+            }
+
+            protected override Expression VisitConditional(ConditionalExpression node)
+            {
+                var equalsOperation = node.Test as BinaryExpression;
+                if (equalsOperation != null)
+                {
+                    var assign = equalsOperation.Left as BinaryExpression;
+                    if (assign != null)
+                    {
+                        var parameter = assign.Left as ParameterExpression;
+                        if (parameter != null && !this.parameters.Contains(parameter))
+                        {
+                            return Expression.Empty();
+                        }
+                    }
+                }
+
+                return base.VisitConditional(node);
+            }
+
+            protected override Expression VisitParameter(ParameterExpression node)
+            {
+                if (this.typeToParameterExpressionTable.ContainsKey(node.Type))
+                {
+                    return base.VisitParameter(this.typeToParameterExpressionTable[node.Type]);
+                }
+
+                return base.VisitParameter(node);
+            }
+        }
+    }
+}
